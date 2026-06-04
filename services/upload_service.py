59130e2fe26c106c0aef226e053975a6707ad496
Eveline Dioos <eveline.dioos@gmail.com>
2026-06-04 19:05:54 +0200

    Document id

--- services/upload_service.py
+++ services/upload_service.py
@@ -1,6 +1,9 @@
+import uuid #uuid kan unieke ID's maken.
+
 from fastapi import UploadFile, HTTPException
 from services.pdf_service import extract_text_from_pdf, get_pdf_page_count
 from services.summarize_service import generate_summary
+from data.document_store import documents
 
 
 async def process_upload(file: UploadFile):
@@ -17,9 +20,13 @@ async def process_upload(file: UploadFile):
         file.file.seek(0)
         text = extract_text_from_pdf(file)
 
+        document_id = str(uuid.uuid4())
+        documents[document_id] = text
+
         summary = generate_summary(text)
 
         return {
+            "document_id": document_id,
             "filename": file.filename,
             "content_type": file.content_type,
             "page_count": page_count,
